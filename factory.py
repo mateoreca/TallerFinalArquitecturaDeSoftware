d5446beea930d12ae107d7d24d2ec2ad01937ca8
s4mooel <zaratesamu99@gmail.com>
2025-11-12 18:30:57 -0500

    agregar archivo factory.py

--- factory.py
+++ factory.py
@@ -1,11 +1,19 @@
+from abc import ABC, abstractmethod
 from prototype import Computadora, Telefono, Tableta
 
-class ProductoFactory:
-    def crear_computadora(self, nombre, precio, especificaciones):
+class ProductoFactory(ABC):
+    @abstractmethod
+    def crear_producto(self, nombre, precio, especificaciones):
+        pass
+
+class ComputadoraFactory(ProductoFactory):
+    def crear_producto(self, nombre, precio, especificaciones):
         return Computadora(nombre, precio, especificaciones)
-    
-    def crear_telefono(self, nombre, precio, especificaciones):
+
+class TelefonoFactory(ProductoFactory):
+    def crear_producto(self, nombre, precio, especificaciones):
         return Telefono(nombre, precio, especificaciones)
-    
-    def crear_tableta(self, nombre, precio, especificaciones):
+
+class TabletaFactory(ProductoFactory):
+    def crear_producto(self, nombre, precio, especificaciones):
         return Tableta(nombre, precio, especificaciones)
